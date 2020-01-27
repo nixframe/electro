15be0fbfcd4e128596bd068f470dbe5a75da3ce8
nixframe <jakub.aussenberg+github@gmail.com>
2020-01-27 12:58:09 +0100

    feature: add simple rest controller

--- src/main/java/pl/example/electro/controller/RestController.java
+++ src/main/java/pl/example/electro/controller/RestController.java
@@ -1,5 +1,27 @@
 package pl.example.electro.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import pl.example.electro.entity.Product;
+import pl.example.electro.service.ProductService;
+import pl.example.electro.service.UserService;
 
+
+@org.springframework.web.bind.annotation.RestController
+@Secured("ROLE_ADMIN")
 public class RestController {
+
+    private ProductService productService;
+
+    @Autowired
+    public RestController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/rest")
+    public Product getProduct(@RequestParam Long id) {
+        return productService.findById(id);
+    }
 }
